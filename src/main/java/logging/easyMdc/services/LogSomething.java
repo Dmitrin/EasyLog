dff827067fcdcf50c2b2be39d6861b8ee7f88a5b
a.dmitrin <dmitrinav@gmail.com>
2018-04-04 12:37:35 +0300

    init

--- src/main/java/logging/easyMdc/services/LogSomething.java
+++ src/main/java/logging/easyMdc/services/LogSomething.java
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Service;
 public class LogSomething implements DoSomething {
 
     public void doSomething() {
-//        log.debug("1");
+        log.debug("1");
+
         System.out.println("Diagnostic MDC content : " + MDC.get("it works"));
 
         System.out.println("Did something!");
 
-
-//        log.debug("2");
+        log.debug("2");
     }
 }
